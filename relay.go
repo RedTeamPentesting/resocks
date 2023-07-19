6ebce8c0596cceeda9400445f575f18a7ee92ec2
Daniel Küppers <daniel.kueppers@redteam-pentesting.de>
2023-07-19 16:06:20 +0200

    Change default behaviour of 0 args to display usage and show error message from only showing error message

--- relay.go
+++ relay.go
@@ -29,7 +29,7 @@ func relayCommand() *cobra.Command {
 		Use:           fmt.Sprintf("%s <connect back address> --key <connection key>", binaryName()),
 		Short:         fmt.Sprintf("Connect back to an %s listener and relay the SOCKS5 traffic", binaryName()),
 		SilenceErrors: true,
-		SilenceUsage:  true,
+		SilenceUsage:  false,
 		Args:          cobraArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			connectBackAddress := defaultConnectBackAddress
